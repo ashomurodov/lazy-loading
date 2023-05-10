8d47de8fe67dce4147c05770309ef9961752bcc7
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-10 16:23:19 +0500

    fix error

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const images = document.querySelectorAll<HTMLImageElement>("img[data-src]");
 const imageBoxes = document.querySelectorAll(".grid-item");
+const btn = document.querySelector("button");
 
 for (let i = 0; i < images.length; i++) {
-
   const newURL = images[i].getAttribute("data-src");
 
   if (newURL) images[i].src = newURL;
@@ -10,5 +10,8 @@ for (let i = 0; i < images.length; i++) {
   images[i].addEventListener("load", () => {
     imageBoxes[i].classList.remove("lazy");
   });
-  
 }
+
+btn?.addEventListener("click", () => {
+  location.reload();
+});
