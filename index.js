250730646f7ea5eb7775e9e8e16fe8f58bdb8c9c
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-12 11:27:12 +0500

    fix mix bug six

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var images = document.querySelectorAll("img[data-src]");
-var imageBoxes = document.querySelectorAll(".grid-item");
-var btn = document.querySelector("button");
-var _loop_1 = function (i) {
-    var newURL = images[i].getAttribute("data-src");
+"use strict";
+const images = document.querySelectorAll("img[data-src]");
+const imageBoxes = document.querySelectorAll(".grid-item");
+const btn = document.querySelector("button");
+const allLazyClass = document.querySelectorAll(".lazy");
+let counter = 0;
+for (let i = 0; i < images.length; i++) {
+    const newURL = images[i].getAttribute("data-src");
     if (newURL)
         images[i].src = newURL;
-    images[i].addEventListener("load", function () {
-        imageBoxes[i].classList.remove("lazy");
+    images[i].addEventListener("load", () => {
+        counter++;
+        imageBoxes[i].classList.add("loaded");
+        if (counter === images.length) {
+            console.log("all images loaded");
+            imageBoxes.forEach((i) => {
+                i.classList.remove("loaded");
+                i.classList.remove("lazy");
+            });
+        }
     });
-};
-for (var i = 0; i < images.length; i++) {
-    _loop_1(i);
 }
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
     location.reload();
 });
--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 const images = document.querySelectorAll<HTMLImageElement>("img[data-src]");
 const imageBoxes = document.querySelectorAll(".grid-item");
 const btn = document.querySelector("button");
+const allLazyClass = document.querySelectorAll<HTMLElement>(".lazy");
 
+let counter = 0;
 for (let i = 0; i < images.length; i++) {
   const newURL = images[i].getAttribute("data-src");
 
   if (newURL) images[i].src = newURL;
 
   images[i].addEventListener("load", () => {
-    imageBoxes[i].classList.remove("lazy");
+    counter++;
+    imageBoxes[i].classList.add("loaded");
+    if (counter === images.length) {
+      console.log("all images loaded");
+      imageBoxes.forEach((i) => {
+        i.classList.remove("loaded");
+        i.classList.remove("lazy");
+      });
+    }
   });
 }
 
